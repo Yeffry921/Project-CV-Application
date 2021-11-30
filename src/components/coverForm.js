7e2955043865242ba57db7b45672ffd558c3db39
yeffry <yeffry45@gmail.com>
2021-11-30 16:21:34 -0500

    fix indentation issue

--- src/components/coverForm.js
+++ src/components/coverForm.js
@@ -7,41 +7,43 @@ class CoverForm extends Component {
   }
  
   render () {
-    const { onChange, values } = this.props
-    return (
-      <form>
-        <div>
-          <h1>Personal Information</h1>
-          <input type='text' placeholder='First Name' name='firstName' onChange={onChange} value={values.firstName}/>
-          <input type='text' placeholder='Last Name' name='lastName' onChange={onChange} value={values.lastName}/>
-          <input type='email' placeholder='Email' name='email' onChange={onChange} value={values.email}/>
-          <input type='text' placeholder='Phone Number' name='phone' onChange={onChange} value={values.phone}/>
-          <textarea placeholder='Description' name='description' onChange={onChange} value={values.description}></textarea>
-        </div>
+    const { onChange, values, onSubmit } = this.props
+      return (
+        <div className='cover-wrapper'>
+          <form onSubmit={onSubmit}>
+            <div>
+              <h3>Personal Information</h3>
+              <input type='text' placeholder='First Name' name='firstName' onChange={onChange} value={values.firstName}/>
+              <input type='text' placeholder='Last Name' name='lastName' onChange={onChange} value={values.lastName}/>
+              <input type='email' placeholder='Email' name='email' onChange={onChange} value={values.email}/>
+              <input type='text' placeholder='Phone Number' name='phone' onChange={onChange} value={values.phone}/>
+              <input placeholder='Description' name='description' onChange={onChange} value={values.description}/>
+            </div>
 
-        <div>
-          <h1>Educational Experience</h1>
-          <input type='text' onChange={onChange} value={values.school} name='school' placeholder='School Name'/>
-          <input type='text' onChange={onChange} value={values.title} name='title' placeholder='Title Of Study'/>
-          <input type='date' onChange={onChange} value={values.fromDate} name='fromDate' placeholder='From'/>
-          <input type='date' onChange={onChange} value={values.toDate} name='toDate' placeholder='To (Leave blank if still attending)'/>
-          <input type='text' onChange={onChange} value={values.location} name='location' placeholder='Location'/>
-        </div>
+            <div>
+              <h3>Educational Experience</h3>
+              <input type='text' onChange={onChange} value={values.school} name='school' placeholder='School Name'/>
+              <input type='text' onChange={onChange} value={values.title} name='title' placeholder='Title Of Study'/>
+              <input type='date' onChange={onChange} value={values.fromDate} name='fromDate' placeholder='From'/>
+              <input type='date' onChange={onChange} value={values.toDate} name='toDate' placeholder='To (Leave blank if still attending)'/>
+              <input type='text' onChange={onChange} value={values.location} name='location' placeholder='Location'/>
+            </div>
 
-        <div>
-          <h1>Practical Experience</h1>
-          <input type='text' onChange={onChange} value={values.company} name='company' placeholder='Company Name'/>
-          <input type='text' onChange={onChange} value={values.companyTitle} name='companyTitle' placeholder='Company Title'/>
-          <input type='date' onChange={onChange} value={values.companyDateStart} name='companyDateStart' placeholder='Date started'/>
-          <input type='date' onChange={onChange} value={values.companyDateEnd} name='companyDateEnd' placeholder='Dated ended (Leave blank if still attending)'/>
-          <textarea placeholder='Main tasks' onChange={onChange} value={values.companyTasks} name='companyTasks'></textarea>
-        </div>
+            <div>
+              <h3>Practical Experience</h3>
+              <input type='text' onChange={onChange} value={values.company} name='company' placeholder='Company Name'/>
+              <input type='text' onChange={onChange} value={values.companyTitle} name='companyTitle' placeholder='Company Title'/>
+              <input type='date' onChange={onChange} value={values.companyDateStart} name='companyDateStart' placeholder='Date started'/>
+              <input type='date' onChange={onChange} value={values.companyDateEnd} name='companyDateEnd' placeholder='Dated ended (Leave blank if still attending)'/>
+              <input placeholder='Main tasks' onChange={onChange} value={values.companyTasks} name='companyTasks'/>
+            </div>
 
-        <div>
-          <button type='submit'>Add</button>
-        </div>
-    </form>
-    )
+            <div>
+              <button type='submit' className='btn'>Add</button>
+            </div>
+          </form>
+      </div>
+      )
   }
 }
 
